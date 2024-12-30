aaf81b9a69ea12071ca59ec252e182e344ad92be
Rutheniumlmw <83409596+Rutheniumlmw@users.noreply.github.com>
2024-12-30 10:47:04 +0800

    feat: support download uploaded documents (#532)
    part of #466
    
    ---------
    
    Co-authored-by: MingWei Liu <lmw@s31.pdsl.local>

--- backend/app/api/main.py
+++ backend/app/api/main.py
@@ -7,6 +7,7 @@ from app.api.routes import (
     user,
     api_key,
     feedback,
+    document,
 )
 from app.api.admin_routes.knowledge_base.routes import (
     router as admin_knowledge_base_router,
@@ -52,6 +53,7 @@ api_router.include_router(chat.router, tags=["chat"])
 api_router.include_router(feedback.router, tags=["chat"])
 api_router.include_router(user.router, tags=["user"])
 api_router.include_router(api_key.router, tags=["auth"])
+api_router.include_router(document.router, tags=["documents"])
 api_router.include_router(admin_chat_engine.router, tags=["admin/chat_engine"])
 api_router.include_router(admin_document_router, tags=["admin/documents"])
 api_router.include_router(admin_feedback.router, tags=["admin/feedback"])
